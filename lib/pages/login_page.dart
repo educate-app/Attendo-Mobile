fef7a068621538b9ca4a338f555484a454ee5350
Bishwajeet Parhi <62933155+2002Bishwajeet@users.noreply.github.com>
2021-10-10 12:15:51 +0530

    added Readme.md

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 enum Status {
   login,
